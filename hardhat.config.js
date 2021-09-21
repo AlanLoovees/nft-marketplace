7fa231954167b1db73f4a721e25bea99d8632784
Alan Loovees <alanloovees@gmail.com>
2021-09-21 22:47:13 +0530

    added etherscan verification

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,10 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 const fs = require("fs")
 
 const projectId = "8cfee2ff3be84d32bfafebe45d4305cf";
 const privateKey = fs.readFileSync(".secret").toString();
+const apiKey = "F53GDI6JQ58PGF3IPQWTSHG9Y83D4SZ6TX";
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -14,6 +16,9 @@ module.exports = {
       url: `https://ropsten.infura.io/v3/${projectId}`,
       accounts: [privateKey]
     }
+  },
+  etherscan: {
+    apiKey: apiKey
   },
   solidity: "0.8.4",
 };
