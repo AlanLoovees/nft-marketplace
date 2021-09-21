c50bf6c417858f450c51fd3d522d5a329c3e0514
Dharwish Raj <drpmiro@gmail.com>
2021-09-21 17:46:55 +0530

    changes in hardhat config's and deployment script

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,22 +17,22 @@ require("@nomiclabs/hardhat-waffle");
 //  * @type import('hardhat/config').HardhatUserConfig
 //  */
 require("@nomiclabs/hardhat-etherscan");
-const projectId = "3bcf3dc6d78a49a1a69e16f0dc4d8c5d";
-const fs = require("fs");
-const privateKey = fs.readFileSync(".secret").toString()
+// const projectId = "3bcf3dc6d78a49a1a69e16f0dc4d8c5d";
+// const fs = require("fs");
+// const privateKey = fs.readFileSync(".secret").toString()
 
 module.exports = {
-  solidity: "0.8.4",
-  network: {
+  networks: {
     hardhat: {
       chainId: 1337
     },
-    testnet: {
-      url: 'https://ropsten.infura.io/v3/${projectId}',
-      accounts: [privateKey]
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/497e2268f4c94a49b0b12d14c75a78ca",
+      accounts: ['0x45553064b1ce64a8d1ec0bbbc94a32e0ca25b1d8e3c3af1d01c5980d377f9f00']
     }
   },
   etherscan: {
       apiKey: "XTCXYZJVXJYHYEEA5B5MU4V3MJDENXC9A2"
-  }
+  },
+  solidity: "0.8.4"
 };
--- test/sample-test.js
+++ test/sample-test.js
@@ -15,7 +15,29 @@ describe("NFT Market", function () {
     listingPrice = listingPrice.toString()
     const auctionPrice = ethers.utils.parseUnits('100', 'ether');
 
-    await nft.createToken
-    
-  });
-});
+    await nft.createToken("https://www.mytokenlocation.com")
+    await nft.createToken("https://www.mytokenlocation2.com")
+
+
+    await market.createMarketItem(nftContractAddress, 1, auctionPrice, { value: listingPrice })
+    await market.createMarketItem(nftContractAddress, 2, auctionPrice, { value: listingPrice })
+
+    const [_, buyerAddress] = await ethers.getSigners()
+
+    await market.connect(buyerAddress).createMarketSale(nftContractAddress, 1, { value: auctionPrice})
+
+    items = await market.fetchMarketItems()
+    items = await Promise.all(items.map(async i => {
+      const tokenUri = await nft.tokenURI(i.tokenId)
+      let item = {
+        price: i.price.toString(),
+        tokenId: i.tokenId.toString(),
+        seller: i.seller,
+        owner: i.owner,
+        tokenUri
+      }
+      return item
+    }))
+    console.log('items: ', items)
+  })
+})
